86fd8ed2c1b18b566440af0369fb651e0143d532
Dabo Ross <daboross@daboross.net>
2014-02-02 00:03:48 -0800

    Initial commit

---
+++ src/net/daboross/helloandroid/HelloActivity.java
@@ -0,0 +1,14 @@
+package net.daboross.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
